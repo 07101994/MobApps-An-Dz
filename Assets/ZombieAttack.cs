3ea9dccca2c9838050a4078c56f5faa9a2faa2e0
Aleksandr Anfilov <aprobinda@gmail.com>
2017-04-19 15:01:46 +0200

    updated Zombie script

--- Assets/ZombieAttack.cs
+++ Assets/ZombieAttack.cs
@@ -2,20 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ZombieAttack : MonoBehaviour {
-
-
-	public Transform mytarget;
+public class ZombieAttack : MonoBehaviour
+{
+	public Transform king;
+	//Spartan King object
 	public GameObject zombie;
-
-
+	//Zombie object
 	private float speed = 0.5f;
-
-
-	//public float health = 100f;
+	//Zombie moving speed
+	private Animation anim;
+	//Zombie object's animation
+	//private float health = 100f;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
+		anim = this.GetComponent<Animation> ();
+		anim.Play ();
+		
+		//TODO: start moving only if the marker has been seen
 
 		//zombie = GameObject.FindWithTag ("enemies");
 		//mytarget = GameObject.FindWithTag ("warrior");
@@ -23,22 +28,15 @@ public class ZombieAttack : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-
-
-		//transform.position = Vector3.MoveTowards(transform.position, target.position, step);
-		transform.position = Vector3.MoveTowards(transform.position, mytarget.position, Time.deltaTime * speed);
-
-	 	this.GetComponent<Animation> ().Play ();
-
-	
-
-		if (transform.position == mytarget.position) {
-
+	void Update ()
+	{
+		transform.position = Vector3.MoveTowards (transform.position, king.position, Time.deltaTime * speed);	//makes Zombie move towards the King
+		if (transform.position == king.position) {
+			Debug.Log ("transform.position == king.position");
 			this.GetComponent<Animation> ().Stop ();
 
 
-			Destroy(gameObject);
+			//Destroy (gameObject);
 		}
 		
 	}
