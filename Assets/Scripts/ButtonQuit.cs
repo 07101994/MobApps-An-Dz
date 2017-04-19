505247edfa81a008c953c50fb19ae72351595246
Aleksandr Anfilov <aprobinda@gmail.com>
2017-04-20 01:31:58 +0200

    added a Quit button

---
+++ Assets/Scripts/ButtonQuit.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonQuit:MonoBehaviour {
+  public void quitApp() {
+    Debug.Log("ButtonQuit has been pressed");
+    Application.Quit();
+  }
+}
--- Assets/Scripts/Spartan.cs
+++ Assets/Scripts/Spartan.cs
@@ -2,114 +2,72 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public class Spartan:MonoBehaviour {
+  public GameObject spartan, Target;
+  private Vector3 normalizeDirection;
+  private float distance, maxDistance, damage;
+  private GameObject[] enemies;
 
-public class Spartan : MonoBehaviour {
+  void Start() {
+    maxDistance = 1.0f;
+    damage = 100;
 
+    //Target = GameObject.FindGameObjectsWithTag ("enemies");
+    //zombie = GameObject.Find ("zombie");
 
-	private Vector3 normalizeDirection;
-	public GameObject SpartanKing;
+    spartan = GameObject.Find("Spartan");
 
+    //normalizeDirection = (target.position - transform.position).normalized;
 
-	public GameObject Target;
+    spartan.GetComponent<Animation>().Play("walk");
 
-	//public float speed = 0.5f;
+    //foreach (GameObject attackTarget in enemies) {
+    //transform.position = Vector3 (transform.position, this.transform.position, speed * Time.deltaTime);
+    //attackTarget.GetComponent<Animation> ().Play ();
+    //}
+  }
 
-	private float distance;
-	private float maxDistance = 1.0f;
-	private float damage = 100;
+  GameObject ClosestZombie() {
+    float closestDistanceSqr = Mathf.Infinity;
+    GameObject bestTarget = null;
+    enemies = GameObject.FindGameObjectsWithTag("enemies");
+    foreach (GameObject objTarget in enemies) {
+      //float d = Vector2.Distance (this.transform.position, obj.transform.position);
+      Vector3 directionToTarget = objTarget.transform.position - this.transform.position;
+      float dSqrToTarget = directionToTarget.sqrMagnitude;
+      if (bestTarget == null || dSqrToTarget < closestDistanceSqr) {
+	closestDistanceSqr = dSqrToTarget;
+	bestTarget = objTarget;
+      }
+    }
+    return bestTarget;
+  }
 
-	GameObject[] enemies;
+  void ApplyDamage(float damage) {
+    print(damage);
+  }
 
-	void Start()
-	{
+  void Update() {
+    //transform.position += normalizeDirection * speed * Time.deltaTime;
 
-
-		//Target = GameObject.FindGameObjectsWithTag ("enemies");
-		//zombie = GameObject.Find ("zombie");
-
-		SpartanKing = GameObject.Find ("SpartanKing");
-
-		//normalizeDirection = (target.position - transform.position).normalized;
-
-
-
-
-
-		//SpartanKing.GetComponent<Animation> ().Play ("walk");
-
-		//foreach (GameObject attackTarget in enemies) {
-			//transform.position = Vector3 (transform.position, this.transform.position, speed * Time.deltaTime);
-			//attackTarget.GetComponent<Animation> ().Play ();
-		//}
-
-
-	}
-
-
-	GameObject ClosestZombie(){
-
-
-
-		enemies = GameObject.FindGameObjectsWithTag ("enemies");
-
-		float closestDistanceSqr = Mathf.Infinity;
-
-		GameObject bestTarget = null;
-
-		foreach(GameObject objTarget in enemies){
-
-			//float d = Vector2.Distance (this.transform.position, obj.transform.position);
-			Vector3 directionToTarget = objTarget.transform.position - this.transform.position;
-
-			float dSqrToTarget = directionToTarget.sqrMagnitude;
-
-			if(bestTarget == null || dSqrToTarget < closestDistanceSqr) {
-				closestDistanceSqr = dSqrToTarget;
-				bestTarget = objTarget;
-			}
-
-		}
-
-		return bestTarget;
-	}
+    //zombie.GetComponent<Animation> ().Play ();
 
 
-	void ApplyDamage(float damage) {
-		print(damage);
-	}
+    //find target with tag
+    Target = ClosestZombie();
+    //transform.position = Vector3.MoveTowards(transform.position, Target.transform.position, Time.deltaTime * speed);
 
-	void Update()
-	{
+    //find distance to target
+    //Todo: execute this only if a Zombie object wasn't destroyed
+    distance = Vector3.Distance(transform.position, Target.transform.position);
+    //Debug.Log (distance);
 
+    //if within damaging range
+    if (distance < maxDistance) {
 
-		//transform.position += normalizeDirection * speed * Time.deltaTime;
-
-		//zombie.GetComponent<Animation> ().Play ();
-
-
-		//find target with tag
-		Target = ClosestZombie ();
-
-		//transform.position = Vector3.MoveTowards(transform.position, Target.transform.position, Time.deltaTime * speed);
-
-		//find distance to target
-		//Todo: execute this only if a Zombie object wasn't destroyed
-		distance = Vector3.Distance(transform.position, Target.transform.position); 
-
-		//Debug.Log (distance);
-
-		//if within damaging range
-		if (distance < maxDistance) {
-
-			//send message to targets script
-			Target.SendMessage ("ApplyDamage", damage);  
-
-			SpartanKing.GetComponent<Animation> ().Play ("attack");
-
-		} 
-
-	}
-
-
-
+      //send message to targets script
+      Target.SendMessage("ApplyDamage", damage);
+      spartan.GetComponent<Animation>().Play("attack");
+    }
+  }
 }
